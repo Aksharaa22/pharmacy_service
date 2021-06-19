ed1fa337d0fcbdc1906857dbd73a4c89eea99971
Aksharaa22 <36600697+Aksharaa22@users.noreply.github.com>
2021-06-19 18:31:29 +0530

    Add files via upload

--- pharma/home/admin.py
+++ pharma/home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from . models import Image
+from . models import Image,Payment
+
 
 admin.site.register(Image)
+admin.site.register(Payment)
