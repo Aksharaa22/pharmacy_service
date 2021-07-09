7eb9e2e98f673ebf6f55adb302ae9bc4e675b111
Aksharaa22 <36600697+Aksharaa22@users.noreply.github.com>
2021-07-09 12:29:40 +0530

    Add files via upload

---
+++ Pharmacy/PharmSupply/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from . models import Payment
+# Register your models here.
+from .models import *
+
+admin.site.register(Register)
+admin.site.register(Image)
+admin.site.register(Medicine)
+admin.site.register(Order)
+admin.site.register(cart)
+admin.site.register(ShippingAddress)
+admin.site.register(Payment)
